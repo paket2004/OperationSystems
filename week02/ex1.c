cd9f51eddb6e350d5cd8387de92fc688077eaf72
paket2004 <103316763+paket2004@users.noreply.github.com>
2023-09-17 12:29:50 +0300

    ex1.c

--- week02/ex1.c
+++ week02/ex1.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+int main() {
+    int a;
+    unsigned short b;
+    signed long int c;
+    float d;
+    double e;
+    a = INT_MAX;
+    b = USHRT_MAX;
+    c = LONG_MAX;
+    d = FLT_MAX;
+    e = DBL_MAX;
+
+    printf("value: %lu", a);
+    printf(" size: %llu\n",sizeof (a));
+
+    printf("value: %d", b);
+    printf(" size: %llu\n",sizeof (b));
+
+    printf("value: %d", c);
+    printf(" size: %llu\n",sizeof (c));
+
+    printf("value: %f", d);
+    printf(" size: %llu\n",sizeof (d));
+
+    printf("value: %f", e);
+    printf(" size: %llu\n",sizeof (e));
+
+    return 0;
+}
 
