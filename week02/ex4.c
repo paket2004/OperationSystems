4f73bf618f358950bd562a841c634ca0cc8d2758
paket2004 <103316763+paket2004@users.noreply.github.com>
2023-09-19 21:54:37 +0300

    Update ex4.c

--- week02/ex4.c
+++ week02/ex4.c
@@ -9,20 +9,24 @@ void count (char string[], char s) {
         }
         i++;
     }
-    printf("%c: %d \n", s, counter);
+    printf("%c: %d ", s, counter);
 
+}
+
+void countAll (char string[]) {
+    for(int i = 0; string[i]; i++){
+        string[i] = tolower(string[i]);
+    }
+    for (int i = 0; string[i]; i++) {
+        count(string,string[i]);
+    }
 }
 int main() {
     int n = 256;
     char string[n];
     char garbage;
     char aim;
-    scanf("%256[^,], %c", string, &aim);
-    for(int i = 0; string[i]; i++){
-        string[i] = tolower(string[i]);
-    }
-    count(string,aim);
-
-
+    scanf("%s", string);
+    countAll(string);
     return 0;
 }
