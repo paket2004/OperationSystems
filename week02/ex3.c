e05fae272055fc15c1318de90f5313b5b5db5a01
paket2004 <103316763+paket2004@users.noreply.github.com>
2023-09-19 15:39:30 +0300

    Update ex3.c

--- week02/ex3.c
+++ week02/ex3.c
@@ -1,49 +1,70 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdbool.h>
 
-void convert (long long int number, int source, int target) {
-    int n = 200;
-    int ans[n];
+void convert(long long int number, int source, int target) {
     bool flag = true;
     long long int newNumber = 0;
+
     if (source != 10) {
         flag = false;
         long long int temp = number;
-        int i = 0;
+        long long int multiplier = 1;
+
         while (temp > 0) {
-            newNumber += temp%10 * pow(source,i);
-            i++;
+            newNumber += (temp % 10) * multiplier;
+            multiplier *= source;
             temp /= 10;
         }
     }
+
     if (flag) {
         newNumber = number;
     }
-    int i = 0;
-    while (newNumber > 0) {
-        ans[i] = newNumber%target;
-        newNumber /= target;
-        i++;
-    }
-    if (i == 0) {
+
+    if (newNumber == 0) {
         printf("0");
-    } else {
-        for (int j = i - 1; j >= 0; j--) {
-            printf("%d", ans[j]);
-        }
+        return;
     }
 
+    if (target == 1) {
+        for (long long int i = 0; i < newNumber; i++) {
+            printf("1");
+        }
+        return;
+    }
+
+    int numDigits = 0;
+    long long int temp = newNumber;
+    while (temp > 0) {
+        temp /= target;
+        numDigits++;
+    }
+
+    for (int i = numDigits - 1; i >= 0; i--) {
+        int targetDigit = 0;
+        long long int temp = newNumber;
+
+        for (int j = 0; j < i; j++) {
+            temp /= target;
+        }
+
+        targetDigit = temp % target;
+        printf("%d", targetDigit);
+    }
 }
+
 int main() {
     long long int number;
     int source;
     int target;
-    scanf("%lld %d %d", &number,&source,&target);
+
+    scanf("%lld %d %d", &number, &source, &target);
+
     if (number < 0 || source < 2 || source > 10 || target < 2 || target > 10) {
-        printf("cannot convert!");
+        printf("Cannot convert!\n");
         return 0;
     }
-    convert(number,source,target);
+
+    convert(number, source, target);
     return 0;
 }
