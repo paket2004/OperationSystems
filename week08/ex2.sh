540debad784a4db06cc37c0a970f86362b923273
paket2004 <103316763+paket2004@users.noreply.github.com>
2023-11-01 14:51:06 +0300

    Update ex2.sh

--- week08/ex2.sh
+++ week08/ex2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+
 gcc pager.c -o pager
-/.pager 4 2
+./pager 4 2 &
+pager_pid=$!
+
 gcc mmu.c -o mmu
-pid_pager = $(pgreep pager)
-gnome-terminal -- bash -c "./pager 4 2; exec bash"
-pid_pager=$(pgrep pager)
-gnome-terminal -- bash -c "./mmu 4 R0 R1 W1 R0 R2 W2 R0 R3 W2 $pid_pager; exec bash"
+./mmu 4 R0 R1 W1 R0 R2 W2 R0 R3 W2 $pager_pid
