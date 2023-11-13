0a62ec1ac5c15f70946961540884d65c5eb84461
paket2004 <103316763+paket2004@users.noreply.github.com>
2023-11-14 01:40:27 +0300

    Create ex25.sh

---
+++ week10/ex25.sh
@@ -0,0 +1,7 @@
+#gedit ex5.txt
+#chmod ugo-w ex5.txt
+#chmod uo+rwx ex5.txt
+#chmod -R g=u ex5.txt
+# 660 means: Owner and group has read and write permissions (6), others have no permissions (0).
+# 775 means Owner and group has read, write, and execute permissions (7). Others have read and execute permissions (5).
+#777 means: Owner,group and others has read, write, and execute permissions (7).
