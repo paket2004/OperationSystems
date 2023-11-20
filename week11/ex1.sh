d8acc94599e7dd86193d14d8b1e3cd65fa57224d
paket2004 <103316763+paket2004@users.noreply.github.com>
2023-11-20 12:39:36 +0300

    Update ex1.sh

--- week11/ex1.sh
+++ week11/ex1.sh
@@ -1,58 +1,39 @@
 #!/bin/bash
 
-# Step 1: Create a file lofs.img not less than 50 MiB
+# Create a file lofs.img not less than 50 MiB (I create 55 Mib)
 fallocate -l 55M lofs.img
 
-# Step 2: Setup a loop device on the created file
+# Setup a loop device on the created file
 loop_device=$(sudo losetup -f)
 sudo losetup $loop_device lofs.img
 
-# Step 3: Create a Loop File System (LOFS) ext4 on the created file
+# Create a Loop File System (LOFS) ext4 on the created file
 sudo mkfs.ext4 $loop_device
 
-# Step 4: Create a new empty directory ./lofsdisk. Mount the created filesystem
+# Create a new empty directory ./lofsdisk. Mount the created filesystem
 mkdir -p ./lofsdisk
 sudo mount $loop_device ./lofsdisk
 
-# Step 5: Add two files file1, file2 to the LOFS
+# Add two files file1 and file2 with my name and surname to the LOFS
 echo "Ilia" | sudo tee ./lofsdisk/file1 > /dev/null
 echo "Mitrokhin" | sudo tee ./lofsdisk/file2 > /dev/null
-
-# Step 6: Define a function get_libs() to return shared libraries
-get_libs() {
-    binary_file="$1"
-    libs=$(ldd "$binary_file" | awk '/=>/ {print $3}')
-    echo "$libs"
-}
-
-# Step 7: Get shared libraries of commands and add them to LOFS
-copy_libs_to_lofs() {
-    binary_name="$1"
-    libs=$(get_libs "$(command -v $binary_name)")
-    libs_dir="./lofsdisk/libs/$binary_name"
-    sudo mkdir -p "$libs_dir"
-    
-    # Copy each shared library to LOFS directory
-    for lib in $libs; do
-        sudo cp --parents "$lib" "./lofsdisk"
-    done
-}
-
+# make a directories bin and libs inside lofsdisk (if they doesn't exist)
 sudo mkdir -p ./lofsdisk/bin
 sudo mkdir -p ./lofsdisk/libs
 
-# Copy executables
+# Copying certain executable files from the /usr/bin/ directory to the current working directory. The executables being copied are bash, cat, echo, and ls. The --parents flag ensures that the directory structure leading to the destination is created if it doesn't exist.
 sudo cp --parents /usr/bin/{bash,cat,echo,ls} ./
 
-# Copy dependencies
+# Copying specific shared libraries from the /lib/x86_64-linux-gnu/ directory to the ./lofsdisk/libs/ directory. The libraries being copied are libtinfo.so.6, libc.so.6, libpcre2-8.so.0, libpthread.so.0. These libraries are necessary for the proper functioning of the executables (bash, cat, echo, ls) that were copied in the previous line.
 
 sudo cp /lib/x86_64-linux-gnu/{libtinfo.so.6,libc.so.6,libpcre2-8.so.0,libpthread.so.0} ./lofsdisk/libs/
 
 sudo cp /lib64/ld-linux-x86-64.so.2 ./lofsdisk/a
-
+# gcc command compiles the ex1.c program, statically links it, includes debugging information and several sanitizers to catch potential issues during runtime. The resulting binary is named ex1 and is placed in the lofsdisk directory.
 gcc ex1.c -o ./lofsdisk/ex1 -static -g -Wall -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow
+# Change root directory of the process to mount point of the created LOFS, run ex1 and save output to ex1.txt
 sudo chroot ./lofsdisk /ex1 > ex1.txt
-
+# Again, compile a program and run it without changing root directory, append output to the ex1.txt
 gcc ex1.c -o ./lofsdisk/ex1 -static -g -Wall -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow
 ./lofsdisk/ex1 >> ex1.txt
 
